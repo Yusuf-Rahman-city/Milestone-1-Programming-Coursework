0d2d03fe2cd2eac294b9ac3cb8e9978ffa14d3fe
Yusuf-Rahman-city <Yusuf.Rahman@city.ac.uk>
2026-02-12 09:12:22 +0000

    Update master.java

--- src/master.java
+++ src/master.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
+public class master {
+    public static void master(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Does this work?");
     }
